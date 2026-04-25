291af1a503a58257b7344d78f5c79f3ef138611d
LunaAstryx <luna.zeul@gmail.com>
2026-04-25 13:56:15 +0200

    fixed smth FOR THE 4TH FVCKING TIME
    Signed-off-by: LunaAstryx <luna.zeul@gmail.com>

--- src/main/java/lunaastryx/chaotic_comfort/item/ModItems.java
+++ src/main/java/lunaastryx/chaotic_comfort/item/ModItems.java
@@ -22,8 +22,6 @@ public class ModItems {
     public static final Item GOLDEN_NETHER_STAR = registerItem("golden_nether_star",
             new Item(new FabricItemSettings().fireproof()));
 
-    public static final Item PUFFER_ARMOR_TRIM = SmithingTemplateItem.of(new Identifier("chaotic_comfort", "puffer_armor_trim"));
-
     public static final Item POMMES_ROT_WEISS_MUSIC_DISC = registerItem("pommes_rot_weiss_music_disc",
             new MusicDiscItem(7, ModSounds.POMMES_ROT_WEISS, new FabricItemSettings().maxCount(1), 61));
 
